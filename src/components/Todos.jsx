db2dc776ee217c12981d956c6f3063e85a6d760b
VanshMehta588 <161753906+VanshMehta588@users.noreply.github.com>
2024-12-17 19:24:38 +0530

    ..

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { removeTodo } from '../features/todo/todoSlice'
+import { removeTodo, updateTodo } from '../features/todo/todoSlice'
 
 export const Todos = () => {
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
+    const [editId, setEditId] = useState(null)
+    const [editText, setEditText] = useState('')
+
+    const handleEdit = (id, text) => {
+        setEditId(id)
+        setEditText(text)
+    }
+
+    const handleUpdate = () => {
+        if (editText.trim()) {
+            dispatch(updateTodo({ id: editId, newText: editText }))
+            setEditId(null)
+            setEditText('')
+        }
+    }
+
     return (
         <>
             <div className="container-fluid p-0 p-md-5 mt-3 mt-md-0">
@@ -15,13 +31,41 @@ export const Todos = () => {
                             <div key={todo.id} className="col-md-4">
                                 <div className="card mb-3">
                                     <div className="card-body d-flex justify-content-between align-items-center">
-                                        <p className="card-text mb-0">{todo.text}</p>
-                                        <button
-                                            className="btn btn-danger btn-sm"
-                                            onClick={() => dispatch(removeTodo(todo.id))}
-                                        >
-                                            X
-                                        </button>
+                                        {editId === todo.id ? (
+                                            <input
+                                                type="text"
+                                                value={editText}
+                                                onChange={(e) => setEditText(e.target.value)}
+                                                className="form-control me-2"
+                                            />
+                                        ) : (
+                                            <p className="card-text mb-0">{todo.text}</p>
+                                        )}
+                                        <div>
+                                            {editId === todo.id ? (
+                                                <button
+                                                    className="btn btn-success btn-sm me-2"
+                                                    onClick={handleUpdate}
+                                                >
+                                                    Save
+                                                </button>
+                                            ) : (
+                                                <>
+                                                    <button
+                                                        className="btn btn-primary btn-sm me-2"
+                                                        onClick={() => handleEdit(todo.id, todo.text)}
+                                                    >
+                                                        Edit
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-danger btn-sm"
+                                                        onClick={() => dispatch(removeTodo(todo.id))}
+                                                    >
+                                                       Delete
+                                                    </button>
+                                                </>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -35,3 +79,4 @@ export const Todos = () => {
         </>
     )
 }
+
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -22,10 +22,17 @@ export const todoSlice = createSlice({
         removeTodo: (state, action) => {
             state.todos = state.todos.filter(todo => todo.id !== action.payload);
         },
-        updateTodo: (state, action) => { }
+        updateTodo: (state, action) => {
+            const { id, newText } = action.payload;
+            const todoToUpdate = state.todos.find(todo => todo.id === id);
+            if (todoToUpdate) {
+                todoToUpdate.text = newText;
+            }
+        }
     }
 })
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
+
